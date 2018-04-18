5694bff8e857e62edac01457ff7798cd4f5d79f2
Alex Allen <alex@kihon.co>
2018-04-18 10:59:04 -0500

    bring back coffeescript

--- gulpFile.js
+++ gulpFile.js
@@ -9,36 +9,34 @@ const uglify = require('gulp-uglify')
 const pump = require('pump')
 const cssCondense = require('gulp-css-condense')
 
-const coffeeSrc = './src/coffeescript/*.coffee'
+const jsSrc = './src/js/*.js'
 const sassSrc = './src/sass/index.sass'
 const pugSrc = './src/pug/*.pug'
 
-gulp.task('build-js-vendor', cb => {
+gulp.task('build-js', cb => {
   pump([
     gulp.src([
       './node_modules/jquery/dist/jquery.js', 
       './node_modules/foundation-sites/js/foundation.core.js',
-      './node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
-      './node_modules/foundation-sites/js/*.js'
+      // './node_modules/foundation-sites/js/foundation.util.mediaQuery.js',
+      './src/js/app.js'
     ]),
     babel(),
-    concat('vendor.js'),
+    concat('app.js'),
     uglify(),
-    gulp.dest('./build/public/scripts/vendor')
+    gulp.dest('./build/public/scripts')
   ], cb)
 })
 
-gulp.task('build-coffee', cb => {
-  // pump([
-  //   gulp.src(coffeeSrc),
-  //   coffee({bare: true}),
-  //   gulp.dest('./build/public/scripts/')
-  // ], cb)
-
-  return gulp.src('./src/coffeescript/app.coffee')
-    .pipe(coffee({bare: true}))
-    .pipe(gulp.dest('./build/public/scripts'))
-})
+// gulp.task('build-coffee', cb => {
+//   pump([
+//     gulp.src(coffeeSrc),
+//     coffee({bare: true}),
+//     concat('app.js'),
+//     uglify(),
+//     gulp.dest('./build/public/scripts/')
+//   ], cb)
+// })
 
 gulp.task('build-sass', cb => {
   sass(sassSrc, {
@@ -59,7 +57,7 @@ gulp.task('build-pug', cb => {
 })
 
 gulp.task('watch', () => {
-  gulp.watch(coffeeSrc, ['build-coffee'])
+  gulp.watch(jsSrc, ['build-coffee'])
   gulp.watch(sassSrc, ['build-sass'])
   gulp.watch(pugSrc, ['build-pug'])
 })
@@ -71,6 +69,6 @@ gulp.task('connect', () => {
   })
 })
 
-gulp.task('build-assets', ['build-pug', 'build-sass', 'build-js-vendor', 'build-coffee'])
+gulp.task('build-assets', ['build-pug', 'build-sass', 'build-js'])
 
-gulp.task('default', ['build-assets', 'connect', 'watch'])
+gulp.task('default', ['build-assets', 'watch', 'connect'])
